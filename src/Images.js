6952e6d8945f35d79829788af9d89a929524bf5a
valeskaperezrretamal <jperezr@udd.cl>
2019-06-24 10:02:18 -0400

    se borran imagenes y se deja solo la del logo

--- src/Images.js
+++ src/Images.js
@@ -5,10 +5,7 @@ import Content from './Content';
 const Images = (props) =>{
     
     return(
-        <React.Fragment>
-            <img key={props.Images.id}src={'https://api.unsplash.com/search/photos${props.Images.fileName}'} alt={props.fileName}/>
-        </React.Fragment>
-
+        " "
     );
 }
 
